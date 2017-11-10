74069ff8967c41d281d32e42da26c691dc2eda5a
sgehin <sfa.gehin@gmail.com>
2017-11-10 13:36:10 +0100

    adding items to an associative array and retrieving them

--- index.php
+++ index.php
@@ -6,48 +6,16 @@
     </head>
     <body>
         <?php
-        //adding items to an array , php is indexing
-        $paper[]="aa";
-        $paper[]="bb";
-        $paper[]="cc";
-        $paper[]="dd";
-        $paper[]="eeeee";
-        print_r($paper); //result Array ( [0] => aa [1] => bb [2] => cc [3] => dd [4] => eeeee )
+        //adding items to an associative array and retrieving them
+        $paper['copier']="Copier & Multipurpose";
+        $paper['inktjet']="Inktjet Printer";
+        $paper['Laser']="Laser Printer";
+        $paper['fax']="Fax Paper";
         
+        echo $paper['inktjet'];
+        // result Inkjet Printer
         echo"<hr>";
-       /* 
-        //adding items to an array using explicit locations
-        $paper[0]="aa";
-        $paper[1]="bb";
-        $paper[2]="cc";
-        $paper[3]="dd";
-        $paper[4]="eeeee";
-        print_r($paper); // result Array ( [0] => aa [1] => bb [2] => cc [3] => dd [4] => eeeee )
-        */
-         echo"<hr>";
-         
-        //adding items to an array and retreiving them
-        $paper[]="Copier";
-        $paper[]="Inkjet";
-        $paper[]="Laser";
-        $paper[]="Photo";
-        $paper[]="fax";
-        
-        for($x=0;$x<count($paper);$x++){
-            echo "$x: $paper[$x]<br>";
-        }
-        /* result
-         * 0: aa
-            1: bb
-            2: cc
-            3: dd
-            4: eeeee
-            5: Copier
-            6: Inkjet
-            7: Laser
-            8: Photo
-            9: fax
-         */
+           
         
         ?>
     </body>
