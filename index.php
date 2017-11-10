8ea3882997d419ecd080752c0e236254bde6adb6
sgehin <sfa.gehin@gmail.com>
2017-11-10 16:20:13 +0100

    Using list function

--- index.php
+++ index.php
@@ -7,19 +7,11 @@
     <body>
         <?php
               
-        $p1 = array("Copier","Inkjet","Laser","Photo");
-        $j=0;
-        foreach($p1 as $item){
-            echo "$j: $item<br>";
-            
-            ++$j;
-        }
-        
-        /* result  0: Copier
-1: Inkjet
-2: Laser
-3: Photo
-        */
+            // Using the list function
+            list($a,$b)=array('alice','Bob');
+            echo "$a<br>$b";
+            // result   alice
+            //          Bob
         ?>
     </body>
 </html>
