87203d0c83a15c674178f921a57ca57d063455d5
sgehin <sfa.gehin@gmail.com>
2017-11-10 13:13:52 +0100

    adding items to an array and retreiving them

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     </head>
     <body>
         <?php
-        //adding items to an array , php is idexing
+        //adding items to an array , php is indexing
         $paper[]="aa";
         $paper[]="bb";
         $paper[]="cc";
@@ -15,14 +15,40 @@
         print_r($paper); //result Array ( [0] => aa [1] => bb [2] => cc [3] => dd [4] => eeeee )
         
         echo"<hr>";
-        
+       /* 
         //adding items to an array using explicit locations
         $paper[0]="aa";
         $paper[1]="bb";
         $paper[2]="cc";
         $paper[3]="dd";
         $paper[4]="eeeee";
-        print_r($paper); // result         
+        print_r($paper); // result Array ( [0] => aa [1] => bb [2] => cc [3] => dd [4] => eeeee )
+        */
+         echo"<hr>";
+         
+        //adding items to an array and retreiving them
+        $paper[]="Copier";
+        $paper[]="Inkjet";
+        $paper[]="Laser";
+        $paper[]="Photo";
+        $paper[]="fax";
+        
+        for($x=0;$x<count($paper);$x++){
+            echo "$x: $paper[$x]<br>";
+        }
+        /* result
+         * 0: aa
+            1: bb
+            2: cc
+            3: dd
+            4: eeeee
+            5: Copier
+            6: Inkjet
+            7: Laser
+            8: Photo
+            9: fax
+         */
+        
         ?>
     </body>
 </html>
