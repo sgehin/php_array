7a392505e9b0ea9f09cc726024c85218cd5d7f9d
sgehin <sfa.gehin@gmail.com>
2017-11-10 12:59:50 +0100

    adding items to an array explicit locations

--- index.php
+++ index.php
@@ -6,13 +6,23 @@
     </head>
     <body>
         <?php
-        //adding items to an array
+        //adding items to an array , php is idexing
         $paper[]="aa";
         $paper[]="bb";
         $paper[]="cc";
         $paper[]="dd";
+        $paper[]="eeeee";
+        print_r($paper); //result Array ( [0] => aa [1] => bb [2] => cc [3] => dd [4] => eeeee )
         
-        print_r($paper);
+        echo"<hr>";
+        
+        //adding items to an array using explicit locations
+        $paper[0]="aa";
+        $paper[1]="bb";
+        $paper[2]="cc";
+        $paper[3]="dd";
+        $paper[4]="eeeee";
+        print_r($paper); // result         
         ?>
     </body>
 </html>
